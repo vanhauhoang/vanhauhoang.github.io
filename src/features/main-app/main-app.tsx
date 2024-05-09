e06002e1e30895872f81cda500b6cf640de200db
pluswhale <69510044+pluswhale@users.noreply.github.com>
2024-05-09 17:00:53 +0300

    add wiggle and layout main screen

--- src/features/main-app/main-app.tsx
+++ src/features/main-app/main-app.tsx
@@ -6,6 +6,7 @@ import fakeSpin from '../../assets/images/spin_mock.png';
 import loaderIcon from '../../assets/images/loader.png';
 import { Typography } from '../../shared/components/typography';
 import { Button } from '../../shared/components/button';
+import giftIcon from '../../assets/images/gift_icon.png';
 
 import styles from './main-app.module.scss';
 
@@ -32,19 +33,66 @@ const MainApp: FC = (): ReactElement => {
                         </Typography>
                     </div>
                 </div>
+
+                <div className={styles.app__exta_spins}>
+                    <div className={styles.app__exta_spins__free_spin}>
+                        <Typography fontSize={isMobile ? '20px' : '40px'}>Free spins</Typography>
+                        <div className={styles.app__exta_spins__free_spin__score}>
+                           <Typography fontSize={isMobile ? '40px' : '50px'} fontFamily="Roundy Rainbows, sans-serif">
+                                    2
+                                </Typography>
+                            <Typography fontSize={isMobile ? '40px' : '50px'} fontFamily="Roundy Rainbows, sans-serif" color="grey">
+                                    |
+                                </Typography>
+                            <Typography fontSize={isMobile ? '40px' : '50px'} fontFamily="Roundy Rainbows, sans-serif" color="grey">
+                                    2
+                                </Typography>
+                        </div>
+                                
+                    </div>
+                    <div className={styles.app__exta_spins__bonus_spin}>
+                        <Typography fontSize={isMobile ? '20px' : '40px'}>Bonus spins</Typography>
+                        <Typography fontSize={isMobile ? '40px' : '50px'} fontFamily="Roundy Rainbows, sans-serif">
+                                0
+                            </Typography>
+                    </div>
+                </div>
+
+
+                <div className={styles.app__invitation}>
+                    <Button
+                    imageLeft={giftIcon}
+                    fontFamily={'Montserrat, sans-serif'}
+                    height={isMobile ? '100px' : '200px'}
+                    textTransform={'none'}
+                    text={'Refer A Friend'}
+                    subText={'Get 3 bonus spins'}
+                    fontWeight={'bolder'}
+                    borderRadius={'30px'}
+                    />
+                    
+                </div>
+
+
+
+
                 <div className={styles.app__footer_connect}>
                     <div className={styles.app__footer_connect_container}>
                         <div className={styles.app__footer_connect_score}>
                             <Typography fontSize={isMobile ? '20px' : '40px'}>Unclaimed whisk</Typography>
-                            <Typography fontSize={isMobile ? '25px' : '50px'} fontFamily="Roundy Rainbows, sans-serif">
+                            <Typography fontSize={isMobile ? '40px' : '50px'} fontFamily="Roundy Rainbows, sans-serif">
                                 10200
                             </Typography>
                         </div>
                         <Button
                             fontFamily={'Montserrat, sans-serif'}
-                            height={'70px'}
+                            height={isMobile ? '50px' : '70px'}
                             fontSize={isMobile ? '20px' : '40px'}
                             text={'Connect wallet'}
+                            fontWeight={'normal'}
+                            width={'fit-content'}
+                            textTransform={'none'}
+
                         />
                     </div>
                 </div>
--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,19 +1,46 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useEffect, useState } from 'react';
 import styles from './button.module.scss';
 
 type Props = {
     text: string;
+    subText?:string;
     fontSize?: string;
     height?: string;
     fontFamily?: string;
+    fontWeight?: string;
+    imageLeft?: string;
+    imageRight?: string;
+    width?: string;
+    textTransform?: any;
+    borderRadius?: string;
+    
 };
 
 export const Button: FC<Props> = (props): ReactElement => {
-    const { text, fontSize = '16px', height = '', fontFamily } = props;
+    const { text, subText, fontSize = '16px', height = '', fontFamily, fontWeight, imageLeft, 
+            imageRight, width = '100%', textTransform='uppercase', borderRadius} = props;
+
+    const [shouldShake, setShouldShake] = useState(false);
+
+    useEffect(() => {
+        const shakeInterval = setInterval(() => {
+            setShouldShake(true);
+            setTimeout(() => {
+                setShouldShake(false);
+            }, 500); // Duration of the shake animation
+        }, 10_000); // Repeat the shake effect every 2 seconds
+
+        return () => clearInterval(shakeInterval);
+    }, []); // Run only once when the component mounts
 
     return (
-        <button style={{ fontSize, height, fontFamily }} className={styles.button}>
-            {text}
+        <button style={{ fontSize, height, fontFamily, fontWeight, width, textTransform, borderRadius }} className={styles.button}>
+            {imageLeft && <img className={`${styles.button__icon_left} ${shouldShake ? styles.shake : ''}`} src={imageLeft}/>}
+            <div className={styles.button__text_conteiner}>   
+                <span className={styles.button__text_conteiner__text}> {text}</span> 
+                <span className={styles.button__text_conteiner__subtext}> {subText}</span>
+            </div>  
+            {imageRight && <img className={styles.button__icon_right} src={imageRight}/>}
         </button>
     );
 };
