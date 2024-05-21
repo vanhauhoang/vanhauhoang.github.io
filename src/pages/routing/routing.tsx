0b51656d7fb2a750e747735203c4bd9d9058f51e
Egor Dultsev <listenmy@yandex.ru>
2024-05-21 22:36:18 +0300

    fix routing

--- src/pages/routing/routing.tsx
+++ src/pages/routing/routing.tsx
@@ -1,19 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, Suspense } from 'react';
 import React from 'react';
+import LoaderScreen from '../../features/loader-screen/LoaderScreen';
 
 const BuyPage = React.lazy(() => import('../buy/buy'));
 const MainPage = React.lazy(() => import('../main/main'));
 
 export const Routing: FC = (): ReactElement => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="whiskers" element={<MainPage />} />
-                <Route path="whiskers/buy" element={<BuyPage />} />
-                <Route path="*" element={<div>Not found</div>} />
-            </Routes>
-        </BrowserRouter>
+        <Suspense fallback={<LoaderScreen />}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="whiskers" element={<MainPage />} />
+                    <Route path="whiskers/buy" element={<BuyPage />} />
+                    <Route path="*" element={<div>Not found</div>} />
+                </Routes>
+            </BrowserRouter>
+        </Suspense>
     );
 };
 
