d5b4857efb2016de94e011d3589f050f1d58fc06
Egor Dultsev <listenmy@yandex.ru>
2024-05-27 16:36:25 +0300

    move audio bg music

--- src/features/main-app/main-app.tsx
+++ src/features/main-app/main-app.tsx
@@ -1,17 +1,29 @@
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useEffect, useRef } from 'react';
 import { SpinTemplate } from '../spin-template/spin-template';
 import { useAppContext } from '../../app/providers/AppContext';
 import { ExtraSpins } from '../extra-spins/extra-spins';
 import { Invitation } from '../invitation/invitation';
 import { Footer } from '../footer/footer';
+import BackgroundSound from '../../assets/sounds/Casino Background Loop.mp3';
 
 import styles from './main-app.module.scss';
 
 const MainApp: FC = (): ReactElement => {
+    const audioRef = useRef<any>(null);
     const { userData, isMobile, isAvailableToSpin } = useAppContext();
+
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.play();
+        }
+    }, []);
 
     return (
         <div className={styles.app__wrapper}>
+            <audio ref={audioRef} autoPlay={true} loop={true}>
+                <source src={BackgroundSound} type="audio/mpeg" />
+                Your browser does not support the audio element.
+            </audio>
             <div className={styles.app__container}>
                 <SpinTemplate
                     isUserLoggedIn={!!userData?.userId}
