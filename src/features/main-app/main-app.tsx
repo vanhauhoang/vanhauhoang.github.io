1897fddea129e54af9af248ed4e320d57d67d190
Egor Dultsev <listenmy@yandex.ru>
2024-05-30 16:17:19 +0300

    delete unused code

--- src/features/main-app/main-app.tsx
+++ src/features/main-app/main-app.tsx
@@ -1,105 +1,16 @@
-import { FC, ReactElement, useRef } from 'react';
+import { FC, ReactElement } from 'react';
 import { SpinTemplate } from '../spin-template/spin-template';
 import { useAppContext } from '../../app/providers/AppContext';
 import { ExtraSpins } from '../extra-spins/extra-spins';
 import { Invitation } from '../invitation/invitation';
 import { Footer } from '../footer/footer';
-import BackgroundSound from '../../assets/sounds/Casino Background Loop.mp3';
 import styles from './main-app.module.scss';
 
 const MainApp: FC = (): ReactElement => {
     const { userData, isMobile, isAvailableToSpin } = useAppContext();
-    // const [isAudioMuted, setIsAudioMuted] = useState<boolean>(true);
-
-    // useEffect(() => {
-    //     const handleUserInteraction = () => {
-    //         if (audioRef.current) {
-    //             audioRef.current.muted = false; // Unmute the audio
-    //             setIsAudioMuted(false);
-    //             audioRef.current.play().catch((error) => {
-    //                 console.error('Error playing audio:', error);
-    //             });
-    //         }
-    //     };
-
-    //     const setupAudio = () => {
-    //         if (audioRef.current) {
-    //             audioRef.current.muted = true; // Ensure audio starts muted
-    //             audioRef.current.play().catch((error) => {
-    //                 console.error('Error playing audio:', error);
-    //             });
-    //         }
-    //     };
-
-    //     // const addInteractionListeners = () => {
-    //     //     document.addEventListener('click', handleUserInteraction);
-    //     //     document.addEventListener('keydown', handleUserInteraction);
-    //     //     document.addEventListener('touchstart', handleUserInteraction);
-    //     // };
-
-    //     // const removeInteractionListeners = () => {
-    //     //     document.removeEventListener('click', handleUserInteraction);
-    //     //     document.removeEventListener('keydown', handleUserInteraction);
-    //     //     document.removeEventListener('touchstart', handleUserInteraction);
-    //     // };
-
-    //     const initializeTelegramWebApp = () => {
-    //         // @ts-ignore
-    //         if (window.Telegram && window.Telegram.WebApp) {
-    //             // @ts-ignore
-    //             window.Telegram.WebApp.ready();
-    //             // @ts-ignore
-    //             window.Telegram.WebApp.onEvent('viewportChanged', () => {
-    //                 setupAudio(); // Re-setup audio on each viewport change
-    //                 // addInteractionListeners(); // Ensure interaction listeners are added
-    //             });
-    //         }
-    //     };
-
-    //     // Setup audio and listeners on initial load
-    //     setupAudio();
-    //     // addInteractionListeners();
-
-    //     // Initialize Telegram Web App
-    //     initializeTelegramWebApp();
-
-    //     // Clean up event listeners on component unmount
-    //     return () => {
-    //         removeInteractionListeners();
-    //     };
-    // }, []);
-
-    // const playAudio = () => {
-    //     if (audioRef.current) {
-    //         audioRef.current.muted = false;
-    //         audioRef.current.play().catch((error) => {
-    //             console.error('Error playing audio:', error);
-    //         });
-    //         setIsAudioMuted(false);
-    //     }
-    // };
-
-    // const muteAudio = () => {
-    //     if (audioRef.current) {
-    //         audioRef.current.muted = true;
-    //         setIsAudioMuted(true);
-    //     }
-    // };
 
     return (
         <div className={styles.app__wrapper}>
-            {/* <div className={styles.app__mute_icon}>
-                {isAudioMuted ? (
-                    <AiOutlineMuted className={styles.icon} onClick={playAudio} />
-                ) : (
-                    <FaVolumeMute className={styles.icon} onClick={muteAudio} />
-                )}
-            </div> */}
-            {/* <audio ref={audioRef} autoPlay={true} loop={true}>
-                <source src={BackgroundSound} type="audio/mpeg" />
-                Your browser does not support the audio element.
-            </audio> */}
-
             <div className={styles.app__container}>
                 <SpinTemplate
                     isUserLoggedIn={!!userData?.userId}
