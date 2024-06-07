45dda2d1110a7d29296b26864b102e5fd496b8fb
0xmusashi <musashihp42@gmail.com>
2024-06-07 08:25:53 +0700

    change backend api

--- src/shared/api/api-config.ts
+++ src/shared/api/api-config.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 //@ts-ignore
-const hostBackendUrl = 'https://whiskers-be-20f5dcb585e5.herokuapp.com/spin-and-earn/';
+const hostBackendUrl = 'https://whisk-bot-be-b1887383233e.herokuapp.com/spin-and-earn/';
 //@ts-ignore
-const localBackendUrl = 'http://localhost:3000/spin-and-earn/';
+const localBackendUrl = 'http://localhost:4000/spin-and-earn/';
 
 export const Instance = axios.create({
     baseURL: hostBackendUrl,
+    // baseURL: localBackendUrl,
     headers: {
         Accept: 'application/json',
         ['Content-Type']: 'application/json',
